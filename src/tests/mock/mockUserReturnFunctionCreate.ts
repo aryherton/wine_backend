ffd105439f5d550837a23135790f8b368fae06df
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 20:51:27 -0300

    feat: created file "mockUserReturnFunctionCreate.ts" in folder "./src/tests/mock"

---
+++ src/tests/mock/mockUserReturnFunctionCreate.ts
@@ -0,0 +1,13 @@
+export default {
+    _id: {
+      $oid: "62b7028859d9b4b2bb1197e0"
+    },
+    name: "Ary",
+    email: "test@gmail.com",
+    password: "123456",
+    arrProduct: [
+      {
+        idProduct: 1,
+      }
+    ]
+  }
