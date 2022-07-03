085a1c2881f49542feb78d0155fb827bde1295cb
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-07-03 20:43:33 -0300

    feat: created file "messageRes.ts" in folder "./src/enum"

---
+++ src/enum/messageRes.ts
@@ -0,0 +1,13 @@
+enum Messages {
+  INCORRECT = 'Incorrect email or password',
+  INVALID = 'All fields must be filled',
+  UPDATE_SUCCESS = 'Product updated successfully',
+  SUCCESS_RECORD = 'User registered successfully',
+  LOGIN_SUCCESS = 'Login realized success',
+  ERROR_SERVER = 'Sorry error found',
+  BODY_IS_REQUIRED = 'Body is required in the request',
+  ALREADY_REGISTERED = 'User already registered',
+  NOT_TOKEN = 'Token is requested'
+}
+
+export default Messages;
