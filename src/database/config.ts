e9c391c00d8f68c219b85c6f5002844cdfcc2ab2
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-19 18:53:12 -0300

    feat: change file "config.ts" in folder "./src/database"

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   user: process.env.MONGO_USER || 'ary',
-  pass: process.env.MONGO_PASSWORD || '123456789',
-  autoIndex: false,
+  pass: process.env.MONGO_PASSWORD || 'qwe123456789',
+  // autoIndex: false,
   dbName: process.env.MONGO_DB_CONTAINER || 'Wine',
 };
--- src/database/model/Connections.ts
+++ src/database/model/Connections.ts
@@ -2,4 +2,4 @@ import { connect } from 'mongoose';
 
 import options from '../config';
 
-export default (url: string = 'mongodb+srv://ary:<123456789>@wine.3eorkec.mongodb.net/?retryWrites=true&w=majority') => connect(url, options);
+export default (url: string = 'mongodb+srv://ary:qwe123456789@wine.3eorkec.mongodb.net/?retryWrites=true&w=majority') => connect(url, options);
