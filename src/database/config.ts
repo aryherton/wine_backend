7389ff839954bf91758750011ef02db791e579ab
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-19 18:07:55 -0300

    feat: connect in mongodb Atals

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  user: process.env.MONGO_USER,
-  pass: process.env.MONGO_PASSWORD,
+  user: process.env.MONGO_USER || 'ary',
+  pass: process.env.MONGO_PASSWORD || '123456789',
   autoIndex: false,
   dbName: process.env.MONGO_DB_CONTAINER || 'Wine',
 };
